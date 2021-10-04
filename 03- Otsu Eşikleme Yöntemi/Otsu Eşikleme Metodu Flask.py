95aa324927225fdce9aa0e783e5b7e811a870fd0
Rumeysa ÜSTÜN <59111328+rumeysaustun@users.noreply.github.com>
2021-10-04 12:44:48 +0300

    Create Otsu Eşikleme Metodu Flask.py

---
+++ 03- Otsu Eşikleme Yöntemi/Otsu Eşikleme Metodu Flask.py
@@ -0,0 +1,24 @@
+#Aşağıdaki kod diğer .htm kodları ile birlikte kullanılmaktadır.
+from flask import Flask, render_template
+from PIL import Image
+
+#Kullanacağımız kütüphaneleri uygulamaya dahil edilir.
+
+app = Flask(__name__)
+
+@app.route("/otsu")
+def otsu():
+    return render_template("otsu.htm")
+
+@app.route("/otsu_gri")
+def otsu_gri():
+    return  render_template("otsu_gri.htm")
+
+@app.route("/otsu_yeni")
+def otsuyeni():
+    return render_template("otsu_yeni.htm")
+
+#HTML sayflarını oluşturduktan sonra template dosyasına atılır.
+
+if __name__ == "__main__":
+    app.run(debug=True)
