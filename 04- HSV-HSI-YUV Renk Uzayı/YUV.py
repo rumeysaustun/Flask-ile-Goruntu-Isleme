076c4ec9e3443146ef37929162d8fb16746c4c8d
Rumeysa ÜSTÜN <59111328+rumeysaustun@users.noreply.github.com>
2021-10-05 14:51:27 +0300

    Update YUV.py

--- 04- HSV-HSI-YUV Renk Uzayı/YUV.py
+++ 04- HSV-HSI-YUV Renk Uzayı/YUV.py
@@ -1,1 +1,26 @@
+from PIL import Image
+import math
+
+def  rgbtoyuv (R, G, B):
+  r=R/255
+  g=G/255 #Değerler normalize hale getirilir.
+  b=B/255
+
+  Y=(0.299*r)+(0.587*g)+(0.114*b)
+  U=0.492*(b-Y)
+  V=0.877*(r-Y)
+
+  return int(Y*255),int(U*255),int(V*255) # y,u,v değerleri 255 ile çarpılarak resim üzerinde gösterilir.
+resim=Image.open("static\img\YUV.jpg").convert("RGB")
+resim_pix = resim.load()
+w=resim.size[0]
+hg=resim.size[1]
+for i in range(w):
+  for j in range(hg):
+      r, g, b = resim.getpixel((i, j))
+      h, s, v = rgbtoyuv(r, g, b)
+      resim_pix[i,j] = (h, s, v)
+resim.save("static\img\YUV_yeni.jpg")
+resim.show()
+
 
