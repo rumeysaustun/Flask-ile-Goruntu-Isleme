3177b8387fa123cb6c7dcbc98bbfc8584240ae47
Rumeysa ÜSTÜN <59111328+rumeysaustun@users.noreply.github.com>
2021-10-07 12:58:37 +0300

    Create Flask.py

---
+++ 07- Canny Kenar Belirleme/Flask.py
@@ -0,0 +1,21 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route("/uygulama")
+def uygulama():
+    return render_template ("uygulama.htm")
+@app.route("/prewitt")
+def prewitt():
+    return render_template ("prewitt.htm")
+@app.route("/sobel")
+def sobel():
+    return render_template ("sobel.htm")
+
+@app.route("/robert")
+def robert():
+    return render_template ("robert.htm")
+
+if __name__ =="__main__":
+
+    app.run(debug=True)
