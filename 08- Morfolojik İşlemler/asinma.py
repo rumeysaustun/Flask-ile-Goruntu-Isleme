d0b6a432419dd8abe01823203108074370490e29
Rumeysa ÜSTÜN <59111328+rumeysaustun@users.noreply.github.com>
2021-10-11 15:21:38 +0300

    Create asinma.py

---
+++ 08- Morfolojik İşlemler/asinma.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img=cv2.imread("C:\\Users\pc\Python Projeleri\morfolojik\static\img\morfolojik.png",0)
+
+kernel=np.ones((5,5),np.uint8)
+
+erosion=cv2.erode(img,kernel,iterations=1)
+
+cv2.imshow("aşınmış resim",erosion)
+
+
+cv2.imwrite("C:\\Users\pc\Python Projeleri\morfolojik\static\img\morfolojik_asinma.jpg",erosion)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
